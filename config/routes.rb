3bcbf677edd57441961ddc03d281853c1a1b67b5
cuzik <cuzikjack@gmail.com>
2019-04-18 15:29:12 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
+  resources :dashboard, only: [:index]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "dashboard#index"
