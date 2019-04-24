5c2b481cc6e467a044b4efa39767799f4c3bf00f
cuzik <cuzikjack@gmail.com>
2019-04-24 15:16:12 -0300

    fix user sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   layout -> { devise_controller? && !user_signed_in? ? 'devise' : 'application' }
+
+  def current_account
+    current_user.account
+  end
 end
