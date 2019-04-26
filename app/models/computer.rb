36a55be756c98bdbdc44bd2cf4d8bf21feaf34d4
cuzik <cuzikjack@gmail.com>
2019-04-26 19:18:46 -0300

    fix select

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -3,6 +3,8 @@ class Computer < ApplicationRecord
   belongs_to :account
   belongs_to :port, optional: true
 
+  has_one :switch, through: :port
+
   validates :name, presence: true, uniqueness: true
   validates :mac_address, presence: true, uniqueness: true
   validates :ip, presence: true, uniqueness: true
